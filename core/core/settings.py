7645a65883f1f143fbe52b170ce4272564fe0ad8
ramed rafie <marf4196@outlook.com>
2023-01-25 18:42:27 +0330

    models added

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tx*bgmt9#=c##^%00b9t6#7e)4f-kld0&n^okk!z)$!6sgqxe^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
--- core/web/models.py
+++ core/web/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Tasks(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class Teeth(models.Model):
+    TOOTH_CHOICES = (
+        ('cleaning', 'cleaning'),
+        ('lorem', 'lorem'),
+        ('ipsom', 'ipsom')
+    )
+
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    tooth1 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth2 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth3 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth4 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth5 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth6 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth7 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth8 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth9 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth10 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth11 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth12 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth13 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth14 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth15 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth16 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth17 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth18 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth19 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth20 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth21 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth22 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth23 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth24 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth25 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth26 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth27 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth28 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth29 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth30 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth31 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
+    tooth32 = models.CharField(max_length=32, choices=TOOTH_CHOICES, default='lorem')
